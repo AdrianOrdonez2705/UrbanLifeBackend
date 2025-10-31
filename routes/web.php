7f009bcfa5168a7aa7da474184fd7acfddc4395b
AdrianGonzo <adrian.gonzales.f@ucb.edu.bo>
2025-10-30 21:51:15 -0400

    Añadida la funcion de enviar emails (TEST)
    Añadí la función de enviar emails
    Cree un .blade para el diseño y contenido del correo
    Añadí una ruta temporal el web.php
    Cambie el driver de sessions de database a file (temporal para desarrollo)

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use Illuminate\Support\Facades\Mail;
+use App\Mail\Test;
+
+Route::get('/probar-correo', function(){
+    $usuario = (object)['nombre'=>'nombre', 'email' => 'ejemplo@gmail.com'];
+    Mail::to($usuario->email)->send(new Test($usuario));
+
+    return 'Correo enviado';
+});
